2e310e51f41a622190ebd1ae16ce71cbbcb389db
techboy-s <65527961+techboy-s@users.noreply.github.com>
2023-04-01 01:30:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //config env
 dotenv.config();
@@ -17,6 +18,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
